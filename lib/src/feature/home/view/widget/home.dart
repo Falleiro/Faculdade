0a75f002afa82a2f0c0e1c5dfa67306c6c79f796
Falleiro <caiofalleiro@gmail.com>
2023-04-19 22:03:28 -0300

    aula 19/04

--- lib/src/feature/home/view/widget/home.dart
+++ lib/src/feature/home/view/widget/home.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:insta_clone/src/component/Minha_app_bar.dart';
 
 class UserHome extends StatelessWidget {
   const UserHome({super.key});
 
+  PreferredSizeWidget _minhabarra(String texto) {
+    return MinhaAppBar(
+      title: Text(
+        texto,
+        style: const TextStyle(
+          color: Colors.black,
+          fontSize: 36,
+        ),
+      ),
+      elevation: 100,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('HOME', style: TextStyle(fontSize: 48))),
+    return Scaffold(
+      appBar: _minhabarra('Home'),
+      body: const Center(child: Text('HOME', style: TextStyle(fontSize: 48))),
     );
   }
 }
--- lib/src/feature/home/view/widget/post.dart
+++ lib/src/feature/home/view/widget/post.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:insta_clone/src/component/Minha_app_bar.dart';
 
 class UserPost extends StatelessWidget {
   const UserPost({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('POST', style: TextStyle(fontSize: 48))),
+    return Scaffold(
+      appBar: MinhaAppBar(title: Text('Texto da home na app bar')),
+      backgroundColor: Colors.blue,
+      body: const Center(
+        child: Text('POST', style: TextStyle(fontSize: 48)),
+      ),
     );
   }
 }
